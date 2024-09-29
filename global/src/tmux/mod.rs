f58e892cefedf78430517922712a9e999c8f96ca
Robert Jackson <me@rwjblue.com>
2024-09-29 18:24:39 -0400

    Expose `setup_tracing` test helper.
    This can be used in tests to get better debugging output when you have a
    test failure.

--- global/src/tmux/mod.rs
+++ global/src/tmux/mod.rs
@@ -436,8 +436,7 @@ mod tests {
     use insta::assert_debug_snapshot;
     use rand::{distributions::Alphanumeric, Rng};
     use tempfile::tempdir;
-    use test_utils::{create_workspace_with_packages, FakeBin, FakePackage};
-    use tracing_subscriber::EnvFilter;
+    use test_utils::{create_workspace_with_packages, setup_tracing, FakeBin, FakePackage};
 
     use crate::build_utils::generate_symlinks;
 
@@ -451,6 +450,7 @@ mod tests {
         socket_name: String,
         _config_file: Option<PathBuf>,
     }
+
     impl TmuxOptions for TestingTmuxOptions {
         fn is_dry_run(&self) -> bool {
             self.dry_run
@@ -539,14 +539,6 @@ mod tests {
             .status()
             .map(|status| status.success())
             .unwrap_or(false)
-    }
-
-    fn setup_tracing() {
-        tracing_subscriber::fmt()
-            .with_env_filter(
-                EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("off")),
-            )
-            .init();
     }
 
     fn build_testing_options() -> TestingTmuxOptions {
